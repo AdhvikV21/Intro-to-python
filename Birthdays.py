055dc16926247ccbd187e0e7f532b8619ee45483
AdhvikV21 <adzydarock@gmail.com>
2025-07-30 20:56:23 +1000

    Add files via upload

---
+++ Birthdays.py
@@ -0,0 +1,10 @@
+friends_birthdays = [
+    ["Bob", "January 15, 2010"],
+    ["Billy", "March 22, 2011"],
+    ["Jack", "July 01, 2010"],
+    ["Lucas", "September 10, 2010"],
+    ["Ben", "November 05, 2010"]
+]
+print("Friends' Birthdays")
+for name, birthday in friends_birthdays:
+    print("(", name, ") (", birthday, ")")
