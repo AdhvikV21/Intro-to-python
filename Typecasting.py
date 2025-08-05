81df7a8bf7d18d3d11caf2679d37e568f9e119d5
AdhvikV21 <adzydarock@gmail.com>
2025-08-05 19:12:53 +1000

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,22 @@
+name="Adhvik"
+Age=15
+Weight=60.5
+Is_student= True
+print("Name:",name)     
+print("Data type of name is",type(name))
+print("age:",Age)
+print("Data type of age is",type(Age))
+print("Weight:",Weight)
+print("Data type of weight is",type(Weight))
+print("Is_student:",Is_student)
+print("Data type of Is_student is", type(Is_student))
+age=str(Age)
+print(age)
+print("Data type of age is",type(age))
+weight=int(Weight)
+print(weight)
+print("Data tyoe iof age is", type(weight))
+
+      
+
+      
