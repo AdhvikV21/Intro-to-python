d3a382201b5accf4692ea155f36b25ed7e7e2ba7
AdhvikV21 <adzydarock@gmail.com>
2025-08-05 21:27:54 +1000

    Add files via upload

---
+++ Congratualtions message.py
@@ -0,0 +1,8 @@
+text=str(input("Enter a string: "))
+#Uppercase String
+#using step value as -1 to iterate in Uppercase
+uppercase_text=text[::-1]
+text=uppercase_text
+print["Uppercase of given string is:"]
+print[text]
+
